4d2fa58dfc6fe6dce8f195e25268d9b9ebf6043a
lyonbot <lyonbot@gmail.com>
2024-11-25 11:41:43 +0800

    chore: refactor proxy connection to agent

---
+++ server/proxy/connectionToAgent.go
@@ -0,0 +1,321 @@
+package proxy
+
+import (
+	"bytes"
+	"context"
+	"encoding/binary"
+	"encoding/hex"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"remote-agent/biz"
+	"remote-agent/server/agent_handler"
+	"remote-agent/utils"
+	"strings"
+	"sync"
+	"sync/atomic"
+	"time"
+
+	"github.com/gorilla/websocket"
+)
+
+type ConnectionToAgent struct {
+	Ctx context.Context
+
+	counter     atomic.Uint32 // connection counter
+	chanToAgent chan<- []byte // send to agent
+	R           sync.Map      // map[uint32]<-chan []byte // receive from agent
+
+	mu              sync.Mutex
+	cond            *sync.Cond
+	status          CTAStatus
+	connectionError error
+}
+
+type CTAStatus int
+
+const (
+	CTAStatusConnecting CTAStatus = iota
+	CTAStatusConnected
+	CTAStatusDisconnected
+)
+
+func NewConnectionToAgent(ctx context.Context, agent_name string, agent_id string, onDisconnected func(error)) *ConnectionToAgent {
+	c := &ConnectionToAgent{
+		Ctx: ctx,
+	}
+	c.cond = sync.NewCond(&c.mu)
+
+	// start a goroutine to communicate with agent
+	go func() {
+		err := c.communicate(agent_name, agent_id)
+		defer onDisconnected(err)
+		c.mu.Lock()
+		defer c.mu.Unlock()
+		c.status = CTAStatusDisconnected
+		c.connectionError = err
+		c.cond.Broadcast()
+	}()
+
+	return c
+}
+
+// (internal) a goroutine that talks to agent. it always returns a error, when connection is closed.
+// when connected, it will update c.status and notify c.cond. but when disconnected, it wont update c.status.
+func (c *ConnectionToAgent) communicate(agent_name string, agent_id string) error {
+	tunnel, err := agent_handler.MakeAgentTunnel(agent_name, agent_id)
+	if err != nil {
+		return err
+	}
+
+	C_from_agent := tunnel.ChFromAgent
+	C_to_agent := tunnel.ChToAgent
+	defer tunnel.Close()
+
+	// notify agent
+	if err := tunnel.NotifyAgent(biz.AgentNotify{
+		Type: "pty",
+	}); err != nil {
+		return err
+	}
+
+	// send a ping
+	ping := []byte{0xff, 'h', 'i'}
+	C_to_agent <- ping
+	select {
+	case <-c.Ctx.Done():
+		return errors.New("parent context canceled")
+	case <-time.After(time.Second * 5):
+		return errors.New("timeout")
+	case pong := <-C_from_agent:
+		if !bytes.Equal(ping, pong) {
+			return errors.New("ping failed. got " + hex.EncodeToString(pong))
+		}
+	}
+
+	// ready!
+	c.chanToAgent = C_to_agent
+
+	c.mu.Lock()
+	c.status = CTAStatusConnected
+	c.cond.Broadcast()
+	c.mu.Unlock()
+
+	// a watchdog will kill the connection if it doesn't respond in 10 minutes
+	isAboutToClean := false
+	killInMinutes := 10 * time.Minute
+	watchdogTicker := time.NewTicker(killInMinutes / 2)
+	defer watchdogTicker.Stop()
+
+	for {
+		select {
+		case <-watchdogTicker.C:
+			if isAboutToClean {
+				return errors.New("watchdog timeout")
+			} else {
+				isAboutToClean = true
+			}
+
+		case data, ok := <-C_from_agent:
+			if len(data) >= 5 {
+				isAboutToClean = false
+				idBytes := data[1:5]
+				id := binary.BigEndian.Uint32(idBytes)
+				if ch, ok := c.R.Load(id); ok && ch != nil {
+					ch.(chan []byte) <- data
+				}
+			}
+			if !ok {
+				return errors.New("tunnel from agent closed")
+			}
+
+		case <-c.Ctx.Done():
+			return c.Ctx.Err()
+		}
+	}
+}
+
+// wait and ensure connection ready. if connection closed or failed, it will return an error.
+func (c *ConnectionToAgent) WaitForReady() error {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	// wait for connection built. then c.status will be CTAStatusConnected, or CTAStatusDisconnected
+	for c.status == CTAStatusConnecting {
+		c.cond.Wait()
+	}
+
+	if c.status == CTAStatusConnected {
+		// good to go
+		return nil
+	}
+
+	err := c.connectionError
+	if err == nil {
+		err = fmt.Errorf("bad connection status: %d", c.status)
+	}
+	return err
+}
+
+func stripHeaders(headers []biz.ProxyHttpHeader) []biz.ProxyHttpHeader {
+	ans := make([]biz.ProxyHttpHeader, 0, len(headers))
+	for _, header := range headers {
+		name := strings.ToLower(header.Name)
+		if (name == "connection" || name == "upgrade" || name == "keep-alive") ||
+			(name == "proxy-connection" || name == "proxy-authorization") ||
+			(name == "sec-websocket-key" || name == "sec-websocket-version" || name == "sec-websocket-extensions" || name == "sec-websocket-accept") {
+			continue
+		}
+		ans = append(ans, header)
+	}
+	return ans
+}
+
+// proxy HTTP request from client to remote via agent.
+// if failed to build a connection, it will return an error, and you shall send "Bad Gateway" response to client when error presents.
+func (c *ConnectionToAgent) HandleRequest(connReq *biz.ProxyHttpRequest, w http.ResponseWriter, r *http.Request) error {
+	id := c.counter.Add(1)
+	idBytes := binary.BigEndian.AppendUint32(nil, id)
+
+	connReq.Headers = stripHeaders(connReq.Headers)
+
+	chanToAgent := c.chanToAgent
+	chanFromAgent := make(chan []byte, 5)
+	c.R.Store(id, chanFromAgent)
+	defer c.R.CompareAndDelete(id, chanFromAgent)
+
+	connReqBytes, _ := connReq.MarshalMsg(nil)
+	chanToAgent <- utils.JoinBytes2(0x23, idBytes, connReqBytes)
+
+	connRes := biz.ProxyHttpResponse{}
+	select { // try to establish a connection on agent
+	case <-c.Ctx.Done():
+		return c.Ctx.Err()
+
+	case <-time.After(time.Second * 60):
+		return errors.New("timeout")
+
+	case recv := <-chanFromAgent:
+		if recv[0] != 0x23 {
+			return errors.New("bad response: expect 0x23 package")
+		}
+		if _, err := connRes.UnmarshalMsg(recv[5:]); err != nil {
+			return errors.New("bad response: " + err.Error())
+		}
+		if connRes.ConnectionError != "" {
+			return errors.New("connection error: " + connRes.ConnectionError)
+		}
+	}
+
+	// ---- websocket connection!
+	if connRes.IsWebSocket {
+		connRes.Headers = stripHeaders(connRes.Headers)
+		wsConn, err := ws.Upgrade(w, r, biz.ToHttpRequestHeaders(connRes.Headers))
+		if err != nil {
+			return err
+		}
+
+		wsConn.SetPingHandler(func(appData string) error {
+			chanToAgent <- (utils.JoinBytes2(0x21, idBytes, []byte{0x09}, []byte(appData)))
+			return nil
+		})
+		wsConn.SetPongHandler(func(appData string) error {
+			chanToAgent <- (utils.JoinBytes2(0x21, idBytes, []byte{0x0a}, []byte(appData)))
+			return nil
+		})
+
+		wsConnClosed := make(chan struct{}, 1)
+		wg := sync.WaitGroup{}
+
+		wg.Add(1)
+		go func() { // agent->http
+			defer wg.Done()
+
+			for {
+				select {
+				case <-wsConnClosed:
+					return
+				case <-c.Ctx.Done():
+					wsConn.Close()
+					return
+				case recv := <-chanFromAgent:
+					if recv[0] == 0x21 {
+						messageType := int(recv[5])
+						wsConn.WriteMessage(messageType, recv[6:])
+					}
+					if recv[0] == 0x22 {
+						wsConn.Close()
+						return
+					}
+				}
+			}
+		}()
+
+		wg.Add(1)
+		go func() { // http->agent
+			defer wg.Done()
+
+			for {
+				messageType, r, err := wsConn.NextReader()
+				if err != nil {
+					break
+				}
+
+				data, err := io.ReadAll(r)
+				if err != nil {
+					break
+				}
+				chanToAgent <- utils.JoinBytes2(0x21, idBytes, []byte{uint8(messageType)}, data)
+			}
+
+			// client disconnected. shall close the proxy too
+			chanToAgent <- utils.JoinBytes2(0x22, idBytes)
+			wsConnClosed <- struct{}{}
+		}()
+
+		wg.Wait()
+		wsConn.Close()
+		return nil
+	}
+
+	// ---- regular http request
+	{ // write response header & status code
+		for _, h := range connRes.Headers {
+			w.Header().Add(h.Name, h.Value)
+		}
+		w.WriteHeader(int(connRes.StatusCode))
+	}
+
+	{ // write body data
+		for {
+			select {
+			case <-r.Context().Done():
+				// http request disconnected
+				chanToAgent <- utils.JoinBytes2(0x22, idBytes)
+				return nil
+			case <-c.Ctx.Done():
+				// agent disconnected
+				return nil
+			case data := <-chanFromAgent:
+				if data[0] == 0x22 {
+					// close connection
+					return nil
+				}
+				if data[0] == 0x21 {
+					// data
+					w.(io.Writer).Write(data[5:])
+					w.(http.Flusher).Flush()
+				}
+			}
+		}
+	}
+}
+
+var ws = websocket.Upgrader{
+	EnableCompression: true,
+	CheckOrigin: func(r *http.Request) bool {
+		// accept all origin -- be good with reverse proxies
+		return true
+	},
+}
--- server/proxy/service.go
+++ server/proxy/service.go
@@ -1,22 +1,11 @@
 package proxy
 
 import (
-	"bytes"
 	"context"
-	"encoding/binary"
-	"encoding/hex"
-	"errors"
 	"io"
 	"net/http"
 	"remote-agent/biz"
-	"remote-agent/server/agent_handler"
-	"remote-agent/utils"
 	"strings"
-	"sync"
-	"sync/atomic"
-	"time"
-
-	"github.com/gorilla/websocket"
 )
 
 type Service struct {
@@ -29,32 +18,24 @@ type Service struct {
 
 	ctx    context.Context
 	cancel context.CancelFunc
-	conn   *connectionToAgent
+	conn   *ConnectionToAgent
 }
 
-func (s *Service) ensureConnected() (c *connectionToAgent, err error) {
+func (s *Service) ensureConnected() (c *ConnectionToAgent, err error) {
 	c = s.conn
 
 	if c == nil {
 		// create new connection
-		ctx, cancel := context.WithCancel(s.ctx)
-
-		c = newConnectionToAgent(ctx)
-		err := c.connect(s.AgentName, s.AgentId, func(err error) {
-			cancel()
+		c = NewConnectionToAgent(s.ctx, s.AgentName, s.AgentId, func(err error) {
 			if c == s.conn {
 				s.conn = nil
 			}
 		})
-		if err != nil {
-			return nil, err
-		}
-
 		s.conn = c
 	}
 
 	// wait for ready
-	if err = c.waitForReady(); err != nil {
+	if err = c.WaitForReady(); err != nil {
 		return nil, err
 	}
 	return c, nil
@@ -129,303 +110,7 @@ func (s *Service) HandleRequest(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// close the connections to agent
 func (s *Service) Dispose() {
 	s.cancel()
-}
-
-// -----------------------------------------------------------------------------
-
-type connectionToAgent struct {
-	Ctx context.Context
-
-	counter     atomic.Uint32 // connection counter
-	chanToAgent chan<- []byte // send to agent
-	R           sync.Map      // map[uint32]<-chan []byte // receive from agent
-
-	mu        sync.Mutex
-	cond      *sync.Cond
-	connected bool
-}
-
-func newConnectionToAgent(ctx context.Context) *connectionToAgent {
-	ans := &connectionToAgent{
-		Ctx: ctx,
-	}
-	ans.cond = sync.NewCond(&ans.mu)
-	return ans
-}
-
-func (c *connectionToAgent) connect(agent_name string, agent_id string, onDisconnected func(error)) error {
-	tunnel, err := agent_handler.MakeAgentTunnel(agent_name, agent_id)
-	if err != nil {
-		onDisconnected(err)
-		return err
-	}
-
-	C_from_agent := tunnel.ChFromAgent
-	C_to_agent := tunnel.ChToAgent
-
-	disconnect := func(err error) {
-		tunnel.Close()
-		onDisconnected(err)
-	}
-
-	// notify agent
-	if err := tunnel.NotifyAgent(biz.AgentNotify{
-		Type: "pty",
-	}); err != nil {
-		disconnect(err)
-		return err
-	}
-
-	// send a ping
-	ping := []byte{0xff, 'h', 'i'}
-	C_to_agent <- ping
-	select {
-	case <-c.Ctx.Done():
-		err := errors.New("parent context canceled")
-		disconnect(err)
-		return err
-	case <-time.After(time.Second * 5):
-		err := errors.New("timeout")
-		disconnect(err)
-		return err
-	case pong := <-C_from_agent:
-		if !bytes.Equal(ping, pong) {
-			err := errors.New("ping failed. got " + hex.EncodeToString(pong))
-			disconnect(err)
-			return err
-		}
-	}
-
-	// ready!
-	c.chanToAgent = C_to_agent
-
-	go func() {
-		defer disconnect(nil)
-
-		isAboutToClean := false
-		watchdogTicker := time.NewTicker(time.Minute * 5) // duration * 2 = kick timeout
-		defer watchdogTicker.Stop()
-
-		for {
-			select {
-			case <-watchdogTicker.C:
-				if isAboutToClean {
-					disconnect(errors.New("watchdog timeout"))
-					return
-				} else {
-					isAboutToClean = true
-				}
-
-			case data, ok := <-C_from_agent:
-				if len(data) >= 5 {
-					isAboutToClean = false
-					idBytes := data[1:5]
-					id := binary.BigEndian.Uint32(idBytes)
-					if ch, ok := c.R.Load(id); ok && ch != nil {
-						ch.(chan []byte) <- data
-					}
-				}
-				if !ok {
-					return
-				}
-
-			case <-c.Ctx.Done():
-				return
-			}
-		}
-	}()
-
-	c.mu.Lock()
-	defer c.mu.Unlock()
-	c.connected = true
-	c.cond.Broadcast()
-	return nil
-}
-
-func (c *connectionToAgent) waitForReady() (err error) {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-
-	for !c.connected {
-		done := make(chan struct{})
-		go func() {
-			select {
-			case <-time.After(time.Second * 5):
-				err = errors.New("timeout")
-				c.cond.Broadcast()
-			case <-c.Ctx.Done():
-				err = c.Ctx.Err()
-				c.cond.Broadcast()
-			case <-done:
-			}
-		}()
-
-		c.cond.Wait()
-		close(done)
-	}
-	return err
-}
-
-func stripHeaders(headers []biz.ProxyHttpHeader) []biz.ProxyHttpHeader {
-	ans := make([]biz.ProxyHttpHeader, 0, len(headers))
-	for _, header := range headers {
-		name := strings.ToLower(header.Name)
-		if (name == "connection" || name == "upgrade" || name == "keep-alive") ||
-			(name == "proxy-connection" || name == "proxy-authorization") ||
-			(name == "sec-websocket-key" || name == "sec-websocket-version" || name == "sec-websocket-extensions" || name == "sec-websocket-accept") {
-			continue
-		}
-		ans = append(ans, header)
-	}
-	return ans
-}
-
-// proxy HTTP request from client to remote via agent.
-// if failed to build a connection, it will return an error, and you shall send "Bad Gateway" response to client when error presents.
-func (c *connectionToAgent) HandleRequest(connReq *biz.ProxyHttpRequest, w http.ResponseWriter, r *http.Request) error {
-	id := c.counter.Add(1)
-	idBytes := binary.BigEndian.AppendUint32(nil, id)
-
-	connReq.Headers = stripHeaders(connReq.Headers)
-
-	chanToAgent := c.chanToAgent
-	chanFromAgent := make(chan []byte, 5)
-	c.R.Store(id, chanFromAgent)
-	defer c.R.CompareAndDelete(id, chanFromAgent)
-
-	connReqBytes, _ := connReq.MarshalMsg(nil)
-	chanToAgent <- utils.JoinBytes2(0x23, idBytes, connReqBytes)
-
-	connRes := biz.ProxyHttpResponse{}
-	select { // try to establish a connection on agent
-	case <-c.Ctx.Done():
-		return c.Ctx.Err()
-
-	case <-time.After(time.Second * 60):
-		return errors.New("timeout")
-
-	case recv := <-chanFromAgent:
-		if recv[0] != 0x23 {
-			return errors.New("bad response: expect 0x23 package")
-		}
-		if _, err := connRes.UnmarshalMsg(recv[5:]); err != nil {
-			return errors.New("bad response: " + err.Error())
-		}
-		if connRes.ConnectionError != "" {
-			return errors.New("connection error: " + connRes.ConnectionError)
-		}
-	}
-
-	// ---- websocket connection!
-	if connRes.IsWebSocket {
-		connRes.Headers = stripHeaders(connRes.Headers)
-		wsConn, err := ws.Upgrade(w, r, biz.ToHttpRequestHeaders(connRes.Headers))
-		if err != nil {
-			return err
-		}
-
-		wsConn.SetPingHandler(func(appData string) error {
-			chanToAgent <- (utils.JoinBytes2(0x21, idBytes, []byte{0x09}, []byte(appData)))
-			return nil
-		})
-		wsConn.SetPongHandler(func(appData string) error {
-			chanToAgent <- (utils.JoinBytes2(0x21, idBytes, []byte{0x0a}, []byte(appData)))
-			return nil
-		})
-
-		wsConnClosed := make(chan struct{}, 1)
-		wg := sync.WaitGroup{}
-
-		wg.Add(1)
-		go func() { // agent->http
-			defer wg.Done()
-
-			for {
-				select {
-				case <-wsConnClosed:
-					return
-				case <-c.Ctx.Done():
-					wsConn.Close()
-					return
-				case recv := <-chanFromAgent:
-					if recv[0] == 0x21 {
-						messageType := int(recv[5])
-						wsConn.WriteMessage(messageType, recv[6:])
-					}
-					if recv[0] == 0x22 {
-						wsConn.Close()
-						return
-					}
-				}
-			}
-		}()
-
-		wg.Add(1)
-		go func() { // http->agent
-			defer wg.Done()
-
-			for {
-				messageType, r, err := wsConn.NextReader()
-				if err != nil {
-					break
-				}
-
-				data, err := io.ReadAll(r)
-				if err != nil {
-					break
-				}
-				chanToAgent <- utils.JoinBytes2(0x21, idBytes, []byte{uint8(messageType)}, data)
-			}
-
-			// client disconnected. shall close the proxy too
-			chanToAgent <- utils.JoinBytes2(0x22, idBytes)
-			wsConnClosed <- struct{}{}
-		}()
-
-		wg.Wait()
-		wsConn.Close()
-		return nil
-	}
-
-	// ---- regular http request
-	{ // write response header & status code
-		for _, h := range connRes.Headers {
-			w.Header().Add(h.Name, h.Value)
-		}
-		w.WriteHeader(int(connRes.StatusCode))
-	}
-
-	{ // write body data
-		for {
-			select {
-			case <-r.Context().Done():
-				// http request disconnected
-				chanToAgent <- utils.JoinBytes2(0x22, idBytes)
-				return nil
-			case <-c.Ctx.Done():
-				// agent disconnected
-				return nil
-			case data := <-chanFromAgent:
-				if data[0] == 0x22 {
-					// close connection
-					return nil
-				}
-				if data[0] == 0x21 {
-					// data
-					w.(io.Writer).Write(data[5:])
-					w.(http.Flusher).Flush()
-				}
-			}
-		}
-	}
-}
-
-var ws = websocket.Upgrader{
-	EnableCompression: true,
-	CheckOrigin: func(r *http.Request) bool {
-		// accept all origin -- be good with reverse proxies
-		return true
-	},
 }
